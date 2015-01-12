80b43be4caaa2b0677a1bc05d15b635ab05d9cb4
Michal Prihoda <michal@prihoda.net>
2015-01-12 21:52:12 +0100

    Added SSH configuration to install.

---
+++ ssh/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if test ! -e ~/.ssh
+then
+	echo "Creating .ssh directory"
+	mkdir ~/.ssh && chmod 0700 ~/.ssh
+fi
+
+for i in config authorized_keys
+do
+	if test ! -e ~/.ssh/$i
+	then
+		echo "Copying $i to .ssh directory"
+		cp ssh/$i ~/.ssh/$i
+	fi
+done
