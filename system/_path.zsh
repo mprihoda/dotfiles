26caed007318c5fda25aae25de60063e5fb7e7c4
Michal Prihoda <michal@prihoda.net>
2015-06-11 20:51:45 +0200

    Updated default path.
    Removed ./bin, as it seems to be pretty unsecure.
    Added /Library/Ruby/bin, as that is where ruby gems executables are installed on El Capitan.

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:$PATH:/usr/local/bin:/usr/local/sbin:$ZSH/bin"
+export PATH="$PATH:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/Library/Ruby/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
