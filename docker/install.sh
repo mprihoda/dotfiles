db39782c4c4eac0769c1c6aa80fd70626102d60c
Michal Prihoda <michal@prihoda.net>
2017-06-16 11:47:38 +0200

    Use lazy loading for dvm and nvm.

---
+++ docker/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if test ! $(which dvm)
+then
+  echo "  Installing dvm for you."
+  brew install dvm > /tmp/dvm-install.log
+fi
+
---
+++ docker/lazyload.zsh
@@ -0,0 +1,6 @@
+dvm() {
+  unset -f dvm
+  [[ -s "$(brew --prefix dvm)/dvm.sh" ]] && source "$(brew --prefix dvm)/dvm.sh"
+  dvm
+}
+
