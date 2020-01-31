fbc742b8328bb5344d49c2d72ffcfe4c83cab9a9
Michal Prihoda <michal@prihoda.net>
2020-01-31 11:20:44 +0100

    Removed sdkman stuff from zshrc, updated installation for macos.

--- java/install.sh
+++ java/install.sh
@@ -1,29 +1,12 @@
 #!/bin/sh
 
-if [ $(uname) = "Darwin" ]
+if [ -e "/etc/debian_version" ]
 then
+  sudo apt install -y default-jdk-headless curl gnupg unzip zip
 
-if test ! $(which jenv)
-then
-  echo "  Installing jenv for you."
-  brew install jenv > /tmp/jenv-install.log
 fi
 
-else
-
-  #if [ ! -d "${HOME}/.jenv" ]
-  #then
-  #  git clone https://github.com/gcuisinier/jenv.git ~/.jenv
-  #fi
-
-  if [ -e "/etc/debian_version" ]
-  then
-    sudo apt install -y default-jdk-headless curl gnupg unzip zip
-
-    if ! [ -e "${HOME}/.sdkman" ]
-    then
-      curl -s "https://get.sdkman.io" | bash
-    fi
-  fi
-
+if ! [ -e "${HOME}/.sdkman" ]
+then
+  curl -s "https://get.sdkman.io" | bash
 fi
