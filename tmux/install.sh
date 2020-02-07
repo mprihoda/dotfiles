e124cfc51581d6cbd7dc7b58853c72b1f5f49b28
Michal Prihoda <michal@prihoda.net>
2020-02-07 10:48:25 +0100

    Fix tmux config to work on both MacOS and Linux.

--- tmux/install.sh
+++ tmux/install.sh
@@ -4,3 +4,8 @@ if [ $(uname) = "Linux" ]
 then
   sudo apt install -y powerline python3-powerline python-powerline
 fi
+
+if [ $(uname) = "Darwin" -a ! -e "${HOME}/.tmux.local.conf" ]
+then
+  echo 'set-option -g default-command "reattach-to-user-namespace -l zsh"' >> "${HOME}/.tmux.local.conf"
+fi
